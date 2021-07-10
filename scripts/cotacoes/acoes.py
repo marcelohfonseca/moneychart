4e6377c60792ae9109cc5f2cc9ad0cefe4b628bd
Marcelo Henrique Fonseca <fonmarcelo@gmail.com>
2021-07-10 14:00:17 -0300

    Atualizacao de config.json e acoes.py

--- scripts/cotacoes/acoes.py
+++ scripts/cotacoes/acoes.py
@@ -1,5 +1,5 @@
 import investpy as inv
-import json 
+import json
 import pandas as pd
 from datetime import date
 
@@ -8,28 +8,47 @@ with open('../config.json') as config_file:
     config = json.load(config_file)
 
 diretorio = '../../dados/cotacoes/acoes/'
-empresas = config['dados']['acoes']['empresas-br']
 incremental = config['periodo']['incremental']
 ano_inicial = config['periodo']['ano-inicial']
 ano_final = date.today().year
 periodo = list(range(int(ano_inicial), int(ano_final) + 1, 1))
+ativos = config['dados']['acoes']['brasil']
+error_list = []
+
+if len(ativos) == 0:
+    ativos = inv.stocks.get_stocks_list(country='Brazil')
 
 # buscar os dados
 def buscar_dados(data_inicial, data_final):
-    cotacoes = pd.DataFrame(columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Currency', 'Company'])
-    for empresa in empresas:
-        df_acoes = pd.DataFrame(inv.get_stock_historical_data(stock=empresa, country='Brazil', from_date=data_inicial, to_date=data_final))
-        df_acoes = df_acoes.assign(Company=empresa)
-        df_acoes.reset_index(inplace=True)
-        cotacoes = cotacoes.append(df_acoes)
-    return cotacoes
+    hist_cotacoes = pd.DataFrame(columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Currency', 'Ticket'])
+    for ativo in ativos:
+        try:
+            df_cotacoes = pd.DataFrame(inv.get_stock_historical_data(stock=ativo, country='Brazil', from_date=data_inicial, to_date=data_final))
+            df_cotacoes = df_cotacoes.assign(Company=ativo)
+            df_cotacoes.reset_index(inplace = True)
+            hist_cotacoes = hist_cotacoes.append(df_cotacoes)
+        except:
+            print(f'Não encontrado nenhum dado para {ativo}')
+            error_list.append(ativo)
+    return hist_cotacoes
 
-def salvar_arquivo(data_frame):
+def salvar_arquivo(tabela):
     # renomear colunas, classificar e exportar os dados
-    data_frame.reset_index(inplace=True, drop=True)
-    data_frame = data_frame.rename(columns ={'Date':'dt_cotacao', 'Open':'vl_abertura', 'High':'vl_alta', 'Low':'vl_baixa', 'Close':'vl_fechamento', 'Volume':'qt_volume', 'Currency':'cd_moeda', 'Company':'cd_empresa'})
-    data_frame.sort_values(by=['dt_cotacao', 'cd_empresa'])
-    data_frame.to_csv(f'{diretorio}{nome_arquivo}.csv', index=False)
+    tabela.reset_index(inplace=True, drop=True)
+    tabela = tabela.rename(columns={'Date':'dt_cotacao', 
+                                    'Open':'vl_abertura', 
+                                    'High':'vl_alta', 
+                                    'Low':'vl_baixa', 
+                                    'Close':'vl_fechamento', 
+                                    'Volume':'qt_volume', 
+                                    'Currency':'cd_moeda', 
+                                    'Ticket':'cd_ativo'})                                            
+    tabela.sort_values(by=['dt_cotacao', 'cd_ativo'])
+    tabela.to_csv(f'{diretorio}{nome_arquivo}.csv', index=False)    
+    
+    if len(error_list) > 0:
+        print(f'Não foi encontrado dados para estes ativos: {error_list}')
+    
     print(f'Arquivo salvo em "{diretorio}{nome_arquivo}.csv".')
 
 if incremental == True:
